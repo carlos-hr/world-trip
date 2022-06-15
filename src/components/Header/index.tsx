bb4de63a473ba4eb55879abcb2e85828ec8dad51
Carlos-hr <carlos.henrique17.chr@gmail.com>
2022-06-14 22:29:34 -0300

    feat(header): creating header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,16 @@
+import { Box, Flex } from "@chakra-ui/react";
+import Image from "next/image";
+import Link from "next/link";
+
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <Flex h="100" justify="center" align="center">
+      <Link href="/">
+        <Box _hover={{ cursor: "pointer" }}>
+          <Image width="180px" height="45px" src="/logo.svg" alt="logo" />
+        </Box>
+      </Link>
+    </Flex>
   );
 };
 
