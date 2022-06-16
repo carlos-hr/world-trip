fe1c30ed9fc89f01ae855a0c82e46ea4887d42dc
Carlos-hr <carlos.henrique17.chr@gmail.com>
2022-06-16 14:03:44 -0300

    feat(slider): creating and styling slider and creating mock design of continent page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
+import { Box, Divider, Flex } from "@chakra-ui/react";
 import type { NextPage } from "next";
-import { Banner, Header, TravelTypes } from "../components";
+import { Banner, ContinentCarousel, Header, TravelTypes } from "../components";
 
 const Home: NextPage = () => {
   return (
@@ -7,6 +8,12 @@ const Home: NextPage = () => {
       <Header />
       <Banner />
       <TravelTypes />
+      <Flex direction="column" justify="center" align="center">
+        <Divider pt="20" w={90} borderBottom="2px solid black" />
+        <Box maxW="1160" pt="24">
+          <ContinentCarousel />
+        </Box>
+      </Flex>
     </div>
   );
 };
